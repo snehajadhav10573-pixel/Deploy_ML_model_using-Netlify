5b4d1f8a001fe69847cf478cc0f626f45d4dd772
snehajadhav10573-pixel <snehajadhav10573@gmail.com>
2026-04-26 15:34:39 +0530

    Update ResidualPlot.js

--- src/Components/ResidualPlot.js
+++ src/Components/ResidualPlot.js
@@ -1,7 +1,6 @@
 import React from "react";
 import {
-  ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip,
-  Legend, ResponsiveContainer, ReferenceLine
+  ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, ReferenceLine
 } from "recharts";
 import "../css/Comparison.css";
 
